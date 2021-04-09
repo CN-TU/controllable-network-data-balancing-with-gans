9828ecddd61db847b6ecddc82d771df99726ea85
Thomas Schmied <thomas.schmied@gmx.net>
2021-04-09 18:44:35 +0200

    refactor + conditional Wasserstein GAN implementation

--- experiment.py
+++ experiment.py
@@ -1,3 +1,8 @@
+"""
+Incorporate the WGAN-GP loss:
+- https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/wgan/wgan.py
+- https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/wgan_gp/wgan_gp.py
+"""
 import io
 import torch
 import numpy as np
@@ -12,9 +17,9 @@ from tensorboardX import SummaryWriter
 from torchvision.transforms import ToTensor
 
 
-class Experiment:
+class BaseExperiment:
 
-    def __init__(self, G, D, G_optimizer, D_optimizer, criterion, model_save_dir=None, log_dir=None, device=None):
+    def __init__(self, G, D, G_optimizer, D_optimizer, criterion=None, model_save_dir=None, log_dir=None, device=None):
         """
 
         Args:
@@ -66,6 +71,105 @@ class Experiment:
             label_weights: None or List. Weights used for random generation of fake labels.
 
         """
+        raise NotImplementedError()
+
+    def evaluate(self, test_loader, col_to_idx, cols_to_plot, step, num_samples=1024, label_weights=None):
+        """
+        Compares generated feature distributions with actual distributions of the given test set.
+        Plots are written to TensorBoard
+
+        Args:
+            test_loader: PyTorch Dataset. Contains the actual flows, i.e.,
+                the real feature distributions to compare against
+            col_to_idx: Dict. Map column names to index.
+            cols_to_plot: List. Names of feature columns to plot.
+            step: Int. Current step/epoch.
+            num_samples: Int. Number of fake samples to generate.
+            label_weights: None or List. Weights used for random generation of fake labels.
+
+        """
+        with torch.no_grad():
+            noise, noise_labels = self.make_noise(num_samples, label_weights)
+            generated_features = self.G(noise, noise_labels)
+
+            for col in cols_to_plot:
+                idx = col_to_idx[col]
+                real = test_loader.X[:, idx]
+                fake = generated_features[:, idx]
+                image = make_image(real, fake.cpu(), col)
+                self.summary_writer.add_image(col, image, step)
+
+    def make_noise(self, num_samples, label_weights=None):
+        noise = torch.FloatTensor(np.random.normal(0, 1, (num_samples, self.latent_dim))).to(self.device)
+        noise_labels = torch.LongTensor(np.random.choice(np.arange(0, self.num_labels),
+                                                         num_samples, p=label_weights)).to(self.device)
+        return noise, noise_labels
+
+    def log_to_output(self, stats, epoch, step, total_steps):
+        stats_str = " | ".join([f"{k}: {v:.5f}" for k, v in stats.items()])
+        print(f"\nEpoch {epoch} | Batch {step}/{total_steps} |  {stats_str}")
+
+    def log_to_tensorboard(self, stats, epoch, step, steps_per_epoch):
+        global_step = epoch * steps_per_epoch + step
+        for k, v in stats.items():
+            self.summary_writer.add_scalar(k, v, global_step=global_step)
+
+    def save_model(self, epoch):
+        torch.save({
+            'G_state_dict': self.G.state_dict(),
+            'D_state_dict': self.D.state_dict(),
+            'optim_G_state_dict': self.G_optimizer.state_dict(),
+            'optim_D_state_dict': self.D_optimizer.state_dict(),
+        }, self.model_save_dir / f"model-{epoch}.pt")
+
+
+def make_image(real, fake, feature_name):
+    """
+    Images cannot be written to TensorBoard directly.
+    Easiest way is to write them to IOBuffer, then convert to PyTorch tensor.
+
+    """
+    sns.displot({"Real": real, "Fake": fake}, kind="kde", common_norm=False, fill=True, height=5, aspect=1.5)
+    plt.title(feature_name)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close("all")
+    buf.seek(0)
+    image = PIL.Image.open(buf)
+    image = ToTensor()(image)
+    return image
+
+
+class CGANExperiment(BaseExperiment):
+
+    def __init__(self, G, D, G_optimizer, D_optimizer, criterion=None, model_save_dir=None, log_dir=None, device=None):
+        """
+        Implement the conditional GAN architecture.
+
+        Args:
+            G: torch.nn.Module. The generator part of the GAN.
+            D: torch.nn.Module. The discriminator part of the GAN.
+            G_optimizer: torch Optimizer. Generator optimizer.
+            D_optimizer: torch Optimizer. Discriminator optimizer.
+            criterion: torch criterion. Loss function to optimizer for.
+            model_save_dir: String. Directory path to save trained model to.
+            log_dir: String. Directory path to log TensorBoard logs to.
+            device: torch.device. Either cpu or gpu device.
+        """
+        super().__init__(G, D, G_optimizer, D_optimizer, criterion, model_save_dir, log_dir, device)
+
+    def train_epoch(self, train_loader, epoch, log_freq=50, log_tensorboard_freq=1, label_weights=None):
+        """
+        Runs a single training epoch.
+
+        Args:
+            train_loader: torch.utils.data.DataLoader. Iterable over dataset.
+            epoch: Int. Current epoch, important for TensorBoard logging.
+            log_freq: Int. Determines the logging frequency for commandline outputs.
+            log_tensorboard_freq: Int. Determines the logging frequency of TensorBoard logs.
+            label_weights: None or List. Weights used for random generation of fake labels.
+
+        """
         total_steps = len(train_loader)
         running_stats = {"G_loss": 0, "D_loss": 0, "D_loss_fake": 0, "D_loss_real": 0}
         with tqdm(total=total_steps, desc="Train loop") as pbar:
@@ -79,7 +183,7 @@ class Experiment:
                 fake = torch.FloatTensor(batch_size, 1).fill_(0.0).to(self.device)
 
                 # train generator
-                generated_features, noise_labels, G_stats = self.fit_generator(real, batch_size,
+                generated_features, noise_labels, G_stats = self.fit_generator(batch_size, real,
                                                                                label_weights=label_weights)
 
                 # train discriminator
@@ -101,11 +205,9 @@ class Experiment:
             stats_epoch = {"epoch_" + k: v / total_steps for k, v in running_stats.items()}
             self.log_to_tensorboard(stats_epoch, epoch, 0, 1)
 
-    def fit_generator(self, real, batch_size, label_weights=None):
+    def fit_generator(self, batch_size, real, label_weights=None):
         self.G_optimizer.zero_grad()
-        noise = torch.FloatTensor(np.random.normal(0, 1, (batch_size, self.latent_dim))).to(self.device)
-        noise_labels = torch.LongTensor(np.random.choice(np.arange(0, self.num_labels),
-                                                         batch_size, p=label_weights)).to(self.device)
+        noise, noise_labels = self.make_noise(batch_size, label_weights)
         generated_features = self.G(noise, noise_labels)
         validity = self.D(generated_features, noise_labels)
         G_loss = self.criterion(validity, real)
@@ -125,64 +227,90 @@ class Experiment:
         self.D_optimizer.step()
         return {"D_loss": D_loss.item(), "D_loss_real": D_loss_real.item(), "D_loss_fake": D_loss_fake.item()}
 
-    def log_to_output(self, stats, epoch, step, total_steps):
-        stats_str = " | ".join([f"{k}: {v:.5f}" for k, v in stats.items()])
-        print(f"\nEpoch {epoch} | Batch {step}/{total_steps} |  {stats_str}")
 
-    def log_to_tensorboard(self, stats, epoch, step, steps_per_epoch):
-        global_step = epoch * steps_per_epoch + step
-        for k, v in stats.items():
-            self.summary_writer.add_scalar(k, v, global_step=global_step)
+class CWGANExperiment(BaseExperiment):
 
-    def save_model(self, epoch):
-        torch.save({
-            'G_state_dict': self.G.state_dict(),
-            'D_state_dict': self.D.state_dict(),
-            'optim_G_state_dict': self.G_optimizer.state_dict(),
-            'optim_D_state_dict': self.D_optimizer.state_dict(),
-        }, self.model_save_dir / f"model-{epoch}.pt")
-
-    def evaluate(self, test_loader, col_to_idx, cols_to_plot, step, num_samples=1024, label_weights=None):
+    def __init__(self, G, D, G_optimizer, D_optimizer, criterion=None, model_save_dir=None, log_dir=None, device=None):
         """
-        Compares generated feature distributions with actual distributions of the given test set.
-        Plots are written to TensorBoard
+        Implement the conditional Wasserstein-GAN architecture.
 
         Args:
-            test_loader: PyTorch Dataset. Contains the actual flows, i.e.,
-                the real feature distributions to compare against
-            col_to_idx: Dict. Map column names to index.
-            cols_to_plot: List. Names of feature columns to plot.
-            step: Int. Current step/epoch.
-            num_samples: Int. Number of fake samples to generate.
+            G: torch.nn.Module. The generator part of the GAN.
+            D: torch.nn.Module. The discriminator part of the GAN.
+            G_optimizer: torch Optimizer. Generator optimizer.
+            D_optimizer: torch Optimizer. Discriminator optimizer.
+            criterion: torch criterion. Loss function to optimizer for.
+            model_save_dir: String. Directory path to save trained model to.
+            log_dir: String. Directory path to log TensorBoard logs to.
+            device: torch.device. Either cpu or gpu device.
+        """
+        super().__init__(G, D, G_optimizer, D_optimizer, criterion, model_save_dir, log_dir, device)
+
+    def train_epoch(self, train_loader, epoch, log_freq=50, log_tensorboard_freq=1,
+                    G_train_freq=5, label_weights=None):
+        """
+        Runs a single training epoch.
+
+        Args:
+            train_loader: torch.utils.data.DataLoader. Iterable over dataset.
+            epoch: Int. Current epoch, important for TensorBoard logging.
+            log_freq: Int. Determines the logging frequency for commandline outputs.
+            log_tensorboard_freq: Int. Determines the logging frequency of TensorBoard logs.
+            G_train_freq:
             label_weights: None or List. Weights used for random generation of fake labels.
 
         """
-        with torch.no_grad():
-            noise = torch.FloatTensor(np.random.normal(0, 1, (num_samples, self.latent_dim))).to(self.device)
-            noise_labels = torch.LongTensor(np.random.choice(np.arange(0, self.num_labels),
-                                                             num_samples, p=label_weights)).to(self.device)
-            generated_features = self.G(noise, noise_labels)
+        total_steps = len(train_loader)
+        running_stats = {"G_loss": 0, "D_loss": 0, "D_loss_fake": 0, "D_loss_real": 0}
+        with tqdm(total=total_steps, desc="Train loop") as pbar:
+            for step, (features, labels) in enumerate(train_loader):
+                features = features.to(self.device)
+                labels = labels.to(self.device)
 
-            for col in cols_to_plot:
-                idx = col_to_idx[col]
-                real = test_loader.X[:, idx]
-                fake = generated_features[:, idx]
-                image = make_image(real, fake.cpu(), col)
-                self.summary_writer.add_image(col, image, step)
+                # ground truths
+                batch_size = features.shape[0]
 
+                noise, noise_labels = self.make_noise(batch_size, label_weights)
+                generated_features = self.G(noise, noise_labels)
 
-def make_image(real, fake, feature_name):
-    """
-    Images cannot be written to TensorBoard directly.
-    Easiest way is to write them to IOBuffer, then convert to PyTorch tensor.
+                # train discriminator
+                D_stats = self.fit_discriminator(features, labels, generated_features, noise_labels)
 
-    """
-    sns.displot({"Real": real, "Fake": fake}, kind="kde", common_norm=False, fill=True, height=5, aspect=1.5)
-    plt.title(feature_name)
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    plt.close("all")
-    buf.seek(0)
-    image = PIL.Image.open(buf)
-    image = ToTensor()(image)
-    return image
+                # train generator
+                if step % G_train_freq == 0:
+                    generated_features, noise_labels, G_stats = self.fit_generator(noise, noise_labels)
+
+                # logging
+                stats = {**G_stats, **D_stats}
+                running_stats = {k: v + stats[k] for k, v in running_stats.items()}
+                if step % log_freq == 0:
+                    self.log_to_output(stats, epoch, step, total_steps)
+
+                if self.log_dir and step % log_tensorboard_freq == 0:
+                    self.log_to_tensorboard(stats, epoch, step, total_steps)
+                pbar.update(1)
+
+        # logs after epoch
+        if self.log_dir:
+            stats_epoch = {"epoch_" + k: v / total_steps for k, v in running_stats.items()}
+            self.log_to_tensorboard(stats_epoch, epoch, 0, 1)
+
+    def fit_generator(self, noise, noise_labels):
+        self.G_optimizer.zero_grad()
+        generated_features = self.G(noise, noise_labels)
+        G_loss = -torch.mean(self.D(generated_features, noise_labels))
+        G_loss.backward()
+        self.G_optimizer.step()
+        return generated_features, noise_labels, {"G_loss": G_loss.item()}
+
+    def fit_discriminator(self, features, labels, generated_features, noise_labels):
+        self.D_optimizer.zero_grad()
+        D_loss_real = -torch.mean(self.D(features.float(), labels))
+        D_loss_fake = torch.mean(self.D(generated_features.detach(), noise_labels))
+        D_loss = D_loss_real + D_loss_fake
+        D_loss.backward()
+        self.D_optimizer.step()
+        # clip weights
+        for p in self.D.parameters():
+            p.data.clamp_(-0.01, 0.01)
+        return {"D_loss": D_loss.item(), "D_loss_real": D_loss_real.item(), "D_loss_fake": D_loss_fake.item()}
--- train_cgan.py
+++ train_cgan.py
@@ -17,7 +17,7 @@ import argparse
 import torch
 from torch.utils import data
 
-from experiment import Experiment
+from experiment import CGANExperiment, CWGANExperiment
 from cgan import Generator, Discriminator
 from cic_ids_17_dataset import CIC17Dataset
 
@@ -32,15 +32,18 @@ if __name__ == '__main__':
     parser.add_argument("--num_labels", type=int, default=14)
     parser.add_argument("--lr", type=float, default=0.0002)
     parser.add_argument("--log_freq", type=int, default=100, help="Write logs to commandline every n steps.")
-    parser.add_argument("--log_tensorboard_freq", type=int, default=100,
-                        help="Write logs to tensorboard every n steps.")
+    parser.add_argument("--log_tensorboard_freq", type=int,
+                        default=100, help="Write logs to TensorBoard every n steps.")
+    parser.add_argument("--use_wgan", action="store_true", help="Indicates if the WGAN architecture should be used.")
     parser.add_argument("--eval_freq", type=int, default=1, help="Evaluate model every n epochs.")
     parser.add_argument("--save_freq", type=int, default=1, help="Save model every n epochs.")
-    parser.add_argument("--log_dir", type=str, default="./tensorboard/cgan", help="Tensorboard log dir.")
-    parser.add_argument("--model_save_dir", type=str, default="./models/cgan")
+    parser.add_argument("--log_dir", type=str, default="./tensorboard", help="TensorBoard log dir.")
+    parser.add_argument("--model_save_dir", type=str, default="./models")
     args = parser.parse_args()
     print(f"Args: {args}")
 
+    log_dir = args.log_dir + "/cwgan" if args.use_wgan else args.log_dir + "/cgan"
+    model_save_dir = args.model_save_dir + "/cwgan" if args.use_wgan else args.model_save_dir + "/cgan"
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
     print("Loading dataset...")
@@ -63,11 +66,15 @@ if __name__ == '__main__':
     D = Discriminator(args.num_features, args.num_labels).to(device)
     G_optimizer = torch.optim.Adam(G.parameters(), lr=args.lr)
     D_optimizer = torch.optim.Adam(D.parameters(), lr=args.lr)
-    # criterion = torch.nn.MSELoss()
-    criterion = torch.nn.BCEWithLogitsLoss()
 
-    exp = Experiment(G, D, G_optimizer, D_optimizer, criterion,
-                     model_save_dir=args.model_save_dir, log_dir=args.log_dir)
+    if args.use_wgan:
+        exp = CWGANExperiment(G, D, G_optimizer, D_optimizer, criterion=None,
+                              model_save_dir=model_save_dir, log_dir=log_dir)
+    else:
+        # criterion = torch.nn.MSELoss()
+        criterion = torch.nn.BCEWithLogitsLoss()
+        exp = CGANExperiment(G, D, G_optimizer, D_optimizer, criterion,
+                             model_save_dir=model_save_dir, log_dir=log_dir)
     print("Generator:\n", G)
     print("Discriminator:\n", D)
 
