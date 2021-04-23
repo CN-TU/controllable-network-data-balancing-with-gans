616f3f728e7999ee5f62d7a266a5fdbc2bbee173
Thomas Schmied <thomas.schmied@gmx.net>
2021-04-23 13:11:21 +0200

    rename

--- networks.py
+++ networks.py
@@ -23,6 +23,56 @@ class Generator(nn.Module):
             return layers
 
         self.model = nn.Sequential(
+            *block(latent_dim + num_labels, 256, normalize=False),
+            *block(256, 256),
+            nn.Linear(256, num_features),
+            nn.Sigmoid()
+        )
+
+    def forward(self, noise, labels):
+        gen_input = torch.cat((noise, self.label_embedding(labels)), -1)
+        out = self.model(gen_input)
+        return out
+
+
+class Discriminator(nn.Module):
+    def __init__(self, num_features, num_labels):
+        super().__init__()
+        self.num_features = num_features
+        self.num_labels = num_labels
+        self.label_embedding = nn.Embedding(num_labels, num_labels)
+
+        self.model = nn.Sequential(
+            nn.Linear(num_features + num_labels, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            # nn.Sigmoid()
+        )
+
+    def forward(self, features, labels):
+        d_in = torch.cat((features, self.label_embedding(labels)), -1)
+        validity = self.model(d_in)
+        return validity
+
+
+class GeneratorBig(nn.Module):
+    def __init__(self, num_features, num_labels, latent_dim=100):
+        super().__init__()
+        self.num_features = num_features
+        self.num_labels = num_labels
+        self.latent_dim = latent_dim
+        self.label_embedding = nn.Embedding(num_labels, num_labels)
+
+        def block(in_dim, out_dim, normalize=True):
+            layers = [nn.Linear(in_dim, out_dim)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_dim, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
             *block(latent_dim + num_labels, 128, normalize=False),
             *block(128, 256),
             *block(256, 512),
@@ -37,7 +87,7 @@ class Generator(nn.Module):
         return out
 
 
-class Discriminator(nn.Module):
+class DiscriminatorBig(nn.Module):
     def __init__(self, num_features, num_labels):
         super().__init__()
         self.num_features = num_features
@@ -61,3 +111,4 @@ class Discriminator(nn.Module):
         d_in = torch.cat((features, self.label_embedding(labels)), -1)
         validity = self.model(d_in)
         return validity
+
